2ddef6b31650825f3ba51373510e220633d3fffc
Artem Zamotaev <artemmori2905@gmail.com>
2025-04-04 22:12:41 +0300

    new functionality in setings

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,8 +68,6 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -115,7 +114,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),  # Указывает путь к общей папке static
+]
+
+LOGIN_URL = '/users/login'
+
+CSRF_TRUSTED_ORIGINS=[
+    'https://f9b9-95-158-48-175.ngrok-free.app'
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
