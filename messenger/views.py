07fc4d8a25fe47ab64c615929393bb05ae53e769
Artem Zamotaev <artemmori2905@gmail.com>
2025-04-25 20:28:16 +0300

    added simple chat func

--- messenger/views.py
+++ messenger/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http  import HttpResponse, HttpResponseBadRequest
+from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from . import models
 from .import forms
 
 # Create your views here.
+
 @login_required
 def chat_view(request):
     chat_group = get_object_or_404(models.ChatGroup, group_name='Chat1')
-    chat_messages = models.GroupMessage.objects.filter(group = chat_group)
+    chat_messages = models.GroupMessage.objects.filter(group=chat_group)
     form = forms.MessageCreateForm()
 
-    if request.htmx:
+    if request.method == 'POST':
         form = forms.MessageCreateForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
             message.author = request.user
             message.group = chat_group
             message.save()
-            context = {
-                'message': message,
-                'user' : request.user,
-            }
-            return render(request, "messenger/partials/chat_message_p.html", context)
 
-    return render(request, "messenger/chat.html", {'chat_messages':chat_messages,'form' : form})
+            if request.headers.get('HX-Request'):
+                html = render_to_string('messenger/partials/chat_message_p.html', {'message': message, 'user': request.user})
+                return HttpResponse(html)
+
+            return redirect('messenger:chat')
+
+    return render(request, "messenger/chat.html", {
+        'chat_messages': chat_messages,
+        'form': form
+    })
